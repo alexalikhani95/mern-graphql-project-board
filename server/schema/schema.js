3ef77c4716849bb1a918714642b506fb31279ed0
Alex Alikhani <alikhanialex@gmail.com>
2022-09-25 21:42:45 +0100

    Adding project type to schema and project fields

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -19,10 +19,35 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+//Project Type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
+
 // Make a query by the id
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType), // list type as its a list of clients
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      // query to fetch a client
+      type: ProjectType,
+      args: { id: { type: GraphQLID } }, //  id arg, to know which client to get
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      }, // The resolver is What to return/respond with
+    },
     clients: {
       type: new GraphQLList(ClientType), // list type as its a list of clients
       resolve(parent, args) {
