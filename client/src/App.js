27c7cab55a58a31f2e5e0fa270082ca63d34a23f
Alex Alikhani <alikhanialex@gmail.com>
2022-09-28 19:50:09 +0100

    Bring in Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import Header from "./components/Header";
+// InMemoryCache will prevent having to reload the page to show new added data (e.g a client)
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <h1>Hello</h1>
+        </div>
+      </ApolloProvider>
     </>
   );
 }
