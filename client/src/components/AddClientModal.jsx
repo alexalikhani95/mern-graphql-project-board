58fe1d6521d0375a0fa0982a487a773077cd50a7
Alex Alikhani <alikhanialex@gmail.com>
2022-09-28 21:23:04 +0100

    Adding a form to create a client in AddClientModal and using the newly created ADD_CLIENT query to add a client

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -1,30 +1,68 @@
 import { useMutation } from "@apollo/client";
 import React, { useState } from "react";
-import { FaUserIcon } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { ADD_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
 const AddClientModal = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update(cache, { data: { addClient } }) {
+      // This is an alternate way to refresh data live on the page to the example in ClientRow (refetchQueries: [{ query: GET_CLIENTS }],)
+      const { clients } = cache.readQuery({ query: GET_CLIENTS }); // read the clients from the cache
+
+      // Write to the clients in the cache and add the new one to the array
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: [...clients, addClient] },
+      });
+    },
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (name === "" || email === "" || phone === "") {
+      return alert("Please fill in all fields");
+    }
+
+    addClient(name, email, phone);
+
+    // Clear the form after adding client
+    setName("");
+    setEmail("");
+    setPhone("");
+  };
+
   return (
     <>
       <button
         type="button"
-        className="btn btn-primary"
+        className="btn btn-secondary"
         data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
+        data-bs-target="#addClientModal"
       >
-        Launch demo modal
+        <div className="d-flex align-items-center">
+          <FaUser className="icon" />
+          <div>Add client</div>
+        </div>
       </button>
 
       <div
         className="modal fade"
-        id="exampleModal"
-        aria-labelledby="exampleModalLabel"
+        id="addClientModal"
+        aria-labelledby="addClientModalLabel"
         aria-hidden="true"
       >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Modal title
+              <h5 className="modal-title" id="addClientModalLabel">
+                Add Client
               </h5>
               <button
                 type="button"
@@ -33,14 +71,42 @@ const AddClientModal = () => {
                 aria-label="Close"
               ></button>
             </div>
-            <div className="modal-body">...</div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
-                Close
-              </button>
-              <button type="button" className="btn btn-primary">
-                Save changes
-              </button>
+            <div className="modal-body">
+              <form onSubmit={onSubmit}>
+                <div className="mb-3">
+                  <label className="form-label">Name</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Email</label>
+                  <input
+                    className="form-control"
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Phone</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="phone"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </div>
+                <button type="submit" data-bs-dismiss="modal" className="btn btn-secondary">
+                  Submit
+                </button>
+              </form>
             </div>
           </div>
         </div>
