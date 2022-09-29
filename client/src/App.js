71e067ecdf579deebe218df8016a7192db0b6816
Alex Alikhani <alikhanialex@gmail.com>
2022-09-29 20:02:55 +0100

    Creating Projects component where projects are fetched from GET_PROJECTS and rendred to a project card

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Clients from "./components/Clients";
 import AddClientModal from "./components/AddClientModal";
+import Projects from "./components/Projects";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -35,6 +36,7 @@ function App() {
         <Header />
         <div className="container">
           <AddClientModal />
+          <Projects />
           <Clients />
         </div>
       </ApolloProvider>
