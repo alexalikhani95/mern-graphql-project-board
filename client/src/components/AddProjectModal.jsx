edc7a08672d658801e141039812f83a53ca26677
Alex Alikhani <alikhanialex@gmail.com>
2022-10-04 18:13:34 +0100

    Creating new ADD_PROJECR mutation to add a project in the AddProjectModal

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -1,13 +1,26 @@
 import { useMutation, useQuery } from "@apollo/client";
 import React, { useState } from "react";
 import { FaList } from "react-icons/fa";
+import { ADD_PROJECT } from "../mutations/projectMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
 
 const AddProjectModal = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [clientId, setClientId] = useState("");
   const [status, setStatus] = useState("new");
+
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, clientId, status },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject] },
+      });
+    },
+  });
 
   // Get Clients for select
   const { loading, error, data } = useQuery(GET_CLIENTS);
@@ -18,6 +31,8 @@ const AddProjectModal = () => {
     if (name === "" || description === "" || status === "") {
       return alert("Please fill in all fields");
     }
+
+    addProject(name, description, clientId, status);
 
     // Clear the form after adding client
     setName("");
