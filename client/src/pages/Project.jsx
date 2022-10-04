ce58b1213828c00368b819549eb038dc525de3a4
Alex Alikhani <alikhanialex@gmail.com>
2022-10-04 19:55:13 +0100

    EditProjectForm created along with new mutation to edit the project details

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import ClientInfo from "../components/ClientInfo";
 import { GET_PROJECT } from "../queries/projectQueries";
 import DeleteProjectButton from "../components/DeleteProjectButton";
+import EditProjectForm from "../components/EditProjectForm";
 
 const Project = () => {
   const { id } = useParams(); // Get the id from the url
@@ -24,6 +25,8 @@ const Project = () => {
           <p className="lead">{data.project.status}</p>
 
           <ClientInfo client={data.project.client} />
+
+          <EditProjectForm project={data.project} />
 
           <DeleteProjectButton projectId={data.project.id} />
         </div>
